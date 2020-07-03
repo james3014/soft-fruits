32e68dcea1ed14ef3e4b1b82713a8a538c06b19e
james3014 <grantj3014@gmail.com>
2020-07-03 12:38:56 +0100

    Updated custom exception class

--- src/main/java/com/renfrewfruit/exception/SoftFruitException.java
+++ src/main/java/com/renfrewfruit/exception/SoftFruitException.java
@@ -1,5 +1,11 @@
 package com.renfrewfruit.exception;
 
+/*
+ * @author James Grant (QWB19204)
+ * @date 13/06/2020
+ * @version 4.0
+ */
+
 /**
  * This class extends the RunTimeException class and allows a custom exception with a specific
  * details on what has went wrong during runtime.
